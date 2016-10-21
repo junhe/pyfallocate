6d251c66c6a87c86fd8548411a39c5cbe76757c2
Daniel Franganillo <dfranganillo@gmail.com>
2016-10-21 16:56:34 +0200

    Changed definition of off_t to "long long" so files bigger than 4GB can be addressed on 32b environments

--- pyfallocate/fallocate_build.py
+++ pyfallocate/fallocate_build.py
@@ -2,16 +2,17 @@ from cffi import FFI
 ffi = FFI()
 
 ffi.set_source("_fallocate",
-    """
-        #include <fcntl.h>
-    """,
-    libraries=[])
+"""
+#include <fcntl.h>
+""",
+               libraries=[])
 
-ffi.cdef("""     // some declarations from the man page
-    typedef long int off_t;
-    typedef long ssize_t;
-    typedef unsigned size_t;
-    int fallocate(int fd, int mode, off_t offset, off_t len);
+ffi.cdef("""
+// some declarations from the man page
+
+typedef long long off_t;
+
+int fallocate(int fd, int mode, off_t offset, off_t len);
 """)
 
 if __name__ == "__main__":
